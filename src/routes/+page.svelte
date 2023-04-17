3ea17a8d503b9af9652db33de16182972a1e052b
robots4life <34193243+robots4life@users.noreply.github.com>
2023-04-17 14:40:35 +0200

    global page styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style global>
+	html {
+		background-color: rgb(0, 40, 78);
+		color: cornsilk;
+	}
+</style>
