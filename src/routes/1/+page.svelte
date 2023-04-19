e270dd8ddbfdc49b8edd2c6d7e6067b292258220
robots4life <34193243+robots4life@users.noreply.github.com>
2023-04-19 12:41:29 +0200

    create slide 2

--- src/routes/1/+page.svelte
+++ src/routes/1/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { CodeBlock } from 'svhighlight';
-
 	import 'highlight.js/styles/base16/dracula.css';
 
 	let jsonData;
---
+++ src/routes/2/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+	import { CodeBlock } from 'svhighlight';
+	import 'highlight.js/styles/base16/dracula.css';
+</script>
